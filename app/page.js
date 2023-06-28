4d05222900cca425d428a2d3ff31a164d1575e19
Ignacio R.J <38332696+Nachrj@users.noreply.github.com>
2023-06-27 22:11:42 -0300

    changed min-height to calculate header height

--- app/page.js
+++ app/page.js
@@ -9,9 +9,9 @@ export default function Home() {
         <button className="w-24 rounded-xl bg-white px-4 py-3 mr-4">Fijar</button>
         <button className="w-24 rounded-xl bg-white px-4 py-3">Borrar</button>
       </div>
-      <div className="flex-grow flex justify-end items-end">
-      <ZoomButtons />
-      <ArrowsButtons />
+      <div className="flex-grow flex justify-end items-end mb-4">
+        <ZoomButtons />
+        <ArrowsButtons />
       </div>
     </main>
   )
